557cda3e45de10009d354de2dec17a497f9c30a2
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2023-04-24 18:32:29 +0700

    add navigation bar

---
+++ components/nav-bar.js
@@ -0,0 +1,34 @@
+import Button, {ButtonType} from "./button";
+import Image from "next/image";
+import styles from "styles/NavBar.module.css";
+
+
+export default function NavBar({
+    buttonContent = 'button',
+    onClickButton,
+    showGreeting,
+    avatarUrl = '/images/avatar.png',
+    name = 'Vo Van Huy'}){
+    return (
+        <nav className={`${styles['nav-bar']} clearfix`}>
+                <div className={styles.logo}>
+                    <Image 
+                    src="/images/logo.png"
+                    width={150}
+                    height={42}
+                    alt="logo"/>
+                </div>
+
+                <div className={styles['nav-item']}>
+                    <Button onClick={onClickButton} type={ButtonType.ORANGE} content={buttonContent}/>
+                </div>
+
+                {showGreeting
+                &&<div className={styles['greeting'] + ' ' +  styles['nav-item']}>
+                    <Image className={styles.avatar} src={avatarUrl} height={30} width={30} alt="avatar"
+                    />
+                    {`Hello, ${name}`}
+                </div>}
+        </nav>
+    );
+}
