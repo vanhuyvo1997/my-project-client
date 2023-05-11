a8b8f67b85ab97420c70d81cc5fc68c22d417009
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2023-05-11 15:19:33 +0700

    add loading component

---
+++ components/loading/loading.js
@@ -0,0 +1,11 @@
+import styles from "./Loading.module.css";
+export default function Loading(){
+    return <div className={styles.loading}>
+        <div className={styles.loader}>
+            
+        </div>
+        <p>
+        loading...
+        </p>
+    </div>
+}
