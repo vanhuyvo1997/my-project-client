12fb265afbc9b11b56bf865fe6feee3abbfc2847
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2023-05-17 17:29:19 +0700

    #4 add label for text input

--- components/text-input/text-input.js
+++ components/text-input/text-input.js
@@ -1,54 +1,70 @@
-import { useState } from 'react';
-import styles from './TextInput.module.css';
-import Button from '../button/button';
-
+import { useState } from "react";
+import styles from "./TextInput.module.css";
+import Button from "../button/button";
 
 export const TextInputType = {
-    PRIMARY:'primary',
-    ORANGE:'orange'
-}
+  PRIMARY: "primary",
+  ORANGE: "orange",
+};
 export default function TextInput({
-    name,
-    iconSrc,
-    placeholder='placeholder',
-    type = TextInputType.PRIMARY,
-    onChange,
-    password,
-    error,
-    value,
-}){
-    const [isPassword, setIsPassword] = useState(password);
-    return (
-        <>
-            <div >
-                <div className={styles['input-text-container']}>
-                    <input className={`${styles['text-input']} ${styles[type]} ${error&&styles['error']}`}
-                        value={value}
-                        name={name}
-                        type={isPassword? 'password': 'text'}
-                        placeholder={placeholder}
-                        onChange={onChange}
-                        style={iconSrc&&{
-                            backgroundImage: `url('${iconSrc}')`,
-                            paddingLeft: '36px',
-                            ...(password&&{paddingRight:'30px'}),
-                            backgroundRepeat:  'no-repeat',
-                            backgroundPosition: `6px 40%`,
-                            backgroundSize: `24px`,
-                            } || 
-                            //if provid no icon src
-                            password&&{paddingRight: '30px'}}
-                        />
-                        {password&&<button 
-                            style={{...({backgroundImage: isPassword? "url('/images/hide-icon.png')":"url('/images/view-icon.png')"}),}}
-                            onClick={(e)=>{
-                                setIsPassword(!isPassword);
-                            }}
-                            className={styles['eye-button']} type='button'/>}
-                </div>
+  name,
+  iconSrc,
+  placeholder = "placeholder",
+  type = TextInputType.PRIMARY,
+  onChange,
+  password,
+  error,
+  value,
+  label,
+}) {
+  const [isPassword, setIsPassword] = useState(password);
+  return (
+    <>
+      <div>
+        <div className={styles["input-text-container"]}>
+          {label && <label for={name} className={styles.label}>{label + ":"}</label>}
+          <input
+            className={`${styles["text-input"]} ${styles[type]} ${
+              error && styles["error"]
+            }`}
+            value={value}
+            name={name}
+            type={isPassword ? "password" : "text"}
+            placeholder={placeholder}
+            onChange={onChange}
+            style={
+              (iconSrc && {
+                backgroundImage: `url('${iconSrc}')`,
+                paddingLeft: "36px",
+                ...(password && { paddingRight: "30px" }),
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: `6px 40%`,
+                backgroundSize: `24px`,
+              }) ||
+              //if provid no icon src
+              (password && { paddingRight: "30px" })
+            }
+          />
+          {password && (
+            <button
+              style={{
+                ...{
+                  backgroundImage: isPassword
+                    ? "url('/images/hide-icon.png')"
+                    : "url('/images/view-icon.png')",
+                },
+              }}
+              onClick={(e) => {
+                setIsPassword(!isPassword);
+              }}
+              className={styles["eye-button"]}
+              type="button"
+            />
+          )}
+        </div>
 
-                    {error&& <small className={styles['error-message']}>{error}</small>}  
-            </div>
-        </>
-    );
+        {error && <small className={styles["error-message"]}>{error}</small>}
+      </div>
+    </>
+  );
 }
