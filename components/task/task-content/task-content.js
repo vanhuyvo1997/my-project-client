c0579920b068f7b78b6e97d5ea53aa5e237fbf34
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2023-06-09 19:58:35 +0700

    #11 add taskcontent

---
+++ components/task/task-content/task-content.js
@@ -0,0 +1,51 @@
+import Button, { ButtonType } from "@my-project/components/button/button";
+import TaskProgressBar from "../task-progress-bar/task-progress-bar";
+import styles from "./TaskContent.module.css";
+import DropdownMenu from "../dropdown-menu/dropdown-menu";
+
+export default function TaskContent({
+  title = "This is title",
+  desciptions = "This is descriptions",
+  status,
+  createdAt,
+  subtasksNum,
+  onClickExpand,
+  onDelete,
+}) {
+  const handleExpand = (event) => {
+    onClickExpand();
+    if (event.target.classList.contains(styles["expanded"])) {
+      event.target.classList.remove(styles["expanded"]);
+    } else {
+      event.target.classList.add(styles["expanded"]);
+    }
+  };
+
+  return (
+    <div className={styles["task"]}>
+      <div className={styles["task-content"]}>
+        <div className={styles.details}>
+          <h5 className={styles.title}>{title}</h5>
+          <p className={styles.desciptions}>{desciptions}</p>
+          <div className={styles["other-info"]}>
+            <span className={styles["progress-bar"]} ><span className={styles["samll-text"]}>Status:</span> <TaskProgressBar status={status} createdAt={createdAt}/></span>
+            <span className={styles["samll-text"]}>
+              Subtasks: <i>{subtasksNum}</i>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={styles["task-action"]}>
+
+        <DropdownMenu>
+          <Button onClick={onDelete} type={ButtonType.ICON_DLETE} content={"Delete"}/>
+          <Button onClick={onDelete} type={ButtonType.ICON_EDIT} content={"Edit"}/>
+        </DropdownMenu>
+        <button
+          className={styles["expand-button"]}
+          onClick={handleExpand}
+        ></button>
+      </div>
+    </div>
+  );
+}
